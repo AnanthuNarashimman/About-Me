205105f2845db4e66d3fde4ca920e0c285020a6c
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2025-01-01 19:52:41 +0530

    First Complete version

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 console.log("JavaScript linked successfully!");
 
+//code for checking whether the page reoloads or loads to display alert box and set body overflow accordingly
+
+window.onload = function() {
+    const pageloaded = sessionStorage.getItem('pageloaded');
+
+    if(pageloaded) {
+        alertbox.style.display = 'none';
+        document.body.style.overflow = 'auto';
+        typewriter();
+    } else {
+        alertbox.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+    }
+}
+
 //code for closing the alertbox and initiating typewritr functionality
 
 const alertbox = document.querySelector('.alertbox');
@@ -19,11 +34,15 @@ donebtn.addEventListener('click', function() {
     } else {
         alert("Fullscreen mode is not supported on this browser.");
     }
+
+    sessionStorage.setItem('pageloaded', 'true');
 })
 
 closebtn.addEventListener('click', function() {
     alertbox.style.display = 'none';
     typewriter();
+
+    sessionStorage.setItem('pageloaded', 'true');
 })
 
 
@@ -69,6 +88,8 @@ function typewriter() {
     }
 
     document.body.style.overflow = 'auto';
+
+
 }
 
 
@@ -119,3 +140,114 @@ colicon.addEventListener('click', function() {
     genicon.classList.remove('activeicon');
     sclion.classList.remove('activeicon');
 })
+
+// script for technical skills
+
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+
+prevDom.onclick = function() {
+    showSlider('prev');
+}
+nextDom.onclick = function() {
+    showSlider('next');
+}
+
+let timeRunning = 2000;
+let runTimeOut;
+let runAutoRun;
+// let timeAutoNext = 7000;
+
+function showSlider(type) {
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if(type === 'next') {
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next');
+    } else {
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        carouselDom.classList.add('prev');
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    },timeRunning)
+
+    // clearTimeout(runAutoRun);
+    // runAutoRun = setTimeout(() => {
+    //     nextDom.click();
+    // }, timeAutoNext);
+}
+
+
+
+// code for text appearance in interest zones
+
+const interestImage = document.querySelectorAll('.interest-zone .izitem img');
+const interestDes = document.querySelectorAll('.interest-zone .izitem h3');
+
+interestImage.forEach((img, index) => {
+    img.addEventListener('mouseover', () => {
+      interestImage[index].style.opacity = '0.8';
+      interestDes[index].style.opacity = '1';
+      interestDes[index].style.transform = 'translateY(0)';
+    });
+  
+    img.addEventListener('mouseleave', () => {
+      interestImage[index].style.opacity = '1';
+      interestDes[index].style.opacity = '0'; 
+      interestDes[index].style.transform = 'translateY(50px)';
+    });
+  });
+
+interestDes.forEach((h3, index) => {
+    h3.addEventListener('mouseover', () => {
+      interestImage[index].style.opacity = '0.8';  
+      interestDes[index].style.opacity = '1';
+      interestDes[index].style.transform = 'translateY(0)';
+    });
+  
+    h3.addEventListener('mouseleave', () => {
+      interestImage[index].style.opacity = '1';
+      interestDes[index].style.opacity = '0'; 
+      interestDes[index].style.transform = 'translateY(50px)';
+    });
+  });
+
+// code for contact page sliding
+
+const contactbtn = document.querySelector('.contact button');
+const gobackbtn = document.querySelector('.contactCard button');
+const contactcard = document.querySelector('.contactCard');
+
+
+contactbtn.addEventListener('click', function() {
+    contactcard.classList.toggle('active');
+    contactbtn.classList.toggle('contactactive');
+})
+
+gobackbtn.addEventListener('click', function() {
+    contactcard.classList.remove('active');
+    contactbtn.classList.remove('contactactive');
+})
+
+window.addEventListener('click', function (e) {
+    const contactcard = document.querySelector('.contactCard');
+    const contactbtn = document.querySelector('.contactbtn');
+
+    if (!e.target.closest('.contactCard') &&!e.target.classList.contains('contactbtn')
+    ) {
+        contactcard.classList.remove('active');
+        contactbtn.classList.remove('contactactive');
+    }
+});
